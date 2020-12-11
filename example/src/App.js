a8360ebcab33f4534faab3818e0dd0e9c89d8882
László Benedek Borbély <laszlo.borbely@rwth-aachen.de>
2020-12-11 19:00:16 +0100

    adjust demo

--- example/src/App.js
+++ example/src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import Puzzle from 'mots-puzzle-wow-react';
 
 const App = () => {
-  return <div className='container'><Puzzle /></div>;
+  return (
+    <div className='container'>
+      <h1>Normal mode:</h1>
+      <Puzzle />
+      <h1>Easy mode:</h1>
+      <Puzzle easy={true} />
+    </div>
+  );
 };
 
 export default App;
