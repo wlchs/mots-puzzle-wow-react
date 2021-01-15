8138dfe68b242c17840b44a79d9b5a6cae8a312d
László Borbély <laci.dj@gmail.com>
2021-01-15 13:11:25 +0100

    implement generator function that does not allow duplicate entities

--- src/generator/index.js
+++ src/generator/index.js
@@ -4,6 +4,7 @@ import Circular from './Circular';
 import Saturation from './Saturation';
 import Shape from './Shape';
 import SceneValidator from '../validator/SceneValidator';
+import { generateEntityGraph } from './EntityGraph';
 
 /**
  * Fisher-Yates (aka Knuth) Shuffle
@@ -33,10 +34,28 @@ function shuffle(arr) {
 }
 
 /**
- * Function to generate a new puzzle
+ * Generate array of 'length' filled with numbers starting from 'from' or 0.
+ * @param len
+ * @param from
+ * @returns {[]}
+ */
+export function range(len, from) {
+  const array = [];
+  const start = from || 0;
+
+  for (let i = start; i < len + start; i += 1) {
+    array.push(i);
+  }
+
+  return array;
+}
+
+/**
+ * Function to generate a new puzzle.
+ * Allows duplicated entities.
  * @returns {Scene}
  */
-function generatePuzzle() {
+export function generatePuzzle() {
   /*
    * Generate trait map
    */
@@ -116,4 +135,90 @@ function generatePuzzle() {
   return scene;
 }
 
-export default generatePuzzle;
+/**
+ * Function to generate a new puzzle.
+ * No duplicate entities allowed
+ * @returns {Scene}
+ */
+export function generatePuzzleV2() {
+  /**
+   * Create puzzle scene
+   * @type {Scene}
+   */
+  const scene = new Scene();
+
+  /**
+   * Generate entity graph
+   * @type {Entity[]}
+   */
+  const { vertices } = generateEntityGraph();
+
+  /**
+   * Random entity id
+   * @type {number}
+   */
+  const uniqueEntityId = Math.floor(Math.random() * vertices.length);
+
+  /**
+   * Unique entity
+   * @type {Entity}
+   */
+  const uniqueEntity = vertices[uniqueEntityId];
+
+  /**
+   * Random trait id
+   * @type {number}
+   */
+  const uniqueTraitId = Math.floor(Math.random() * uniqueEntity.traits.length);
+
+  /**
+   * Unique trait
+   * @type {Trait}
+   */
+  const uniqueTrait = uniqueEntity.traits[uniqueTraitId];
+
+  /*
+   * Set unique trait
+   */
+  uniqueTrait.setUnique();
+
+  /**
+   * Remove entities with conflicting traits
+   * @type {Entity[]}
+   */
+  const remainingVertices = vertices.filter(
+    (v) =>
+      v.traits[uniqueTraitId].selectedOption !== uniqueTrait.selectedOption,
+  );
+
+  /**
+   * Pick the other three entities randomly
+   * @type {Entity[]}
+   */
+  const otherEntities = shuffle(remainingVertices).slice(0, 3);
+
+  /*
+   * Assign entity set
+   */
+  scene.setEntities([uniqueEntity, ...otherEntities]);
+
+  /**
+   * Get entity array
+   * @type {Entity[]}
+   */
+  const entities = scene.getEntities();
+
+  /*
+   * Shuffle entity array
+   */
+  scene.setEntities(shuffle(entities));
+
+  /*
+   * Validate the generated puzzle scene
+   */
+  if (!SceneValidator.validate(scene)) {
+    throw new Error('Invalid puzzle!');
+  }
+
+  return scene;
+}
